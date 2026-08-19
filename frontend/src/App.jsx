23df1a73326cf55b4443e63cf1e604cb9f31e42c
Anand Kishore Reddy <anandkishorereddy111@gmail.com>
2026-08-19 18:37:49 +0530

    Fix case-sensitive import for Landingpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import LoginPage from "./pages/auth/LoginPage";
 import RegisterPage from "./pages/auth/RegisterPage";
-import LandingPage from "./pages/public/LandingPage";
+import LandingPage from "./pages/public/Landingpage";
 
 import ProtectedRoute from "./routes/ProtectedRoute";
 import AdminRoutes from "./routes/AdminRoutes";
